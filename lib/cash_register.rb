da34044fce40e0e92bb1aed437ed8d01fbd7f00c
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-01 14:18:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -40,8 +40,7 @@ class CashRegister
   
   def void_last_transaction
     if @items.length >= 1
-      new_total = @total - @items.last
-      new_total
+      @total - @items.last
     else
       0.0 
     end
